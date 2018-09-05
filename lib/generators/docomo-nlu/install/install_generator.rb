4e3cec42a494436217617f2179898619aff891bf
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2018-09-05 11:05:06 +0900

    change namespace

--- lib/generators/docomo-nlu/install/install_generator.rb
+++ lib/generators/docomo-nlu/install/install_generator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module DocomoNlu
-  class InstallGenerator < ::Rails::Generators::Base
-    source_root File.expand_path('../../templates', __dir__)
-    def copy_initializer
-      copy_file 'template.rb', 'config/initializers/docomo-nlu.rb'
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path('../../templates', __dir__)
+      def copy_initializer
+        copy_file 'template.rb', 'config/initializers/docomo-nlu.rb'
+      end
     end
   end
 end
