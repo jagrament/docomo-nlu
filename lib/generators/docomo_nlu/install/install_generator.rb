f607eb2ea2fd2b7d2f216574ee2175b7321e2bce
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2018-09-05 11:20:25 +0900

    generater test

--- lib/generators/docomo_nlu/install/install_generator.rb
+++ lib/generators/docomo_nlu/install/install_generator.rb
@@ -4,8 +4,7 @@ module DocomoNlu
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../../templates', __dir__)
 
-    desc << 'Description:
-      Copy docomo-nlu files to your application. please add your key to this file.'
+    desc 'Description: Copy docomo-nlu files to your application. please add your key to this file.'
 
     def copy_initializer
       copy_file 'template.rb', 'config/initializers/docomo-nlu.rb'
