c4d6032791c70affedf4d82aae2f865eaf09c5d8
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2018-09-06 01:49:35 +0900

    Change file name of generated install command

--- lib/generators/docomo_nlu/install/install_generator.rb
+++ lib/generators/docomo_nlu/install/install_generator.rb
@@ -3,9 +3,9 @@
 module DocomoNlu
   class InstallGenerator < ::Rails::Generators::Base
     source_root File.expand_path('../../templates', __dir__)
-    
+
     def copy_initializer
-      copy_file 'template.rb', 'config/initializers/docomo-nlu.rb'
+      copy_file 'template.rb', 'config/initializers/docomo_nlu.rb'
     end
   end
 end
