5e2a7b84c2f66e8f39664b4d977b22e8b29edb4e
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2018-09-05 10:45:03 +0900

    change path

--- lib/generators/docomo-nlu/install/install_generator.rb
+++ lib/generators/docomo-nlu/install/install_generator.rb
@@ -2,7 +2,7 @@
 
 module DocomoNlu
   class InstallGenerator < ::Rails::Generators::Base
-    source_root File.expand_path('../templates', __dir__)
+    source_root File.expand_path('../../templates', __dir__)
     def copy_initializer
       copy_file 'template.rb', 'config/initializers/docomo-nlu.rb'
     end
