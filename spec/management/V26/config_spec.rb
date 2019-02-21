df9ded84b8c04481804f3cb1dd64e5d5906be459
Atsuko Mori <atsuko.mori.200@gmail.com>
2019-02-21 11:47:44 +0900

    Updated the vcr data of Management::V26::Config

--- spec/management/V26/config_spec.rb
+++ spec/management/V26/config_spec.rb
@@ -6,22 +6,25 @@ RSpec.describe DocomoNlu::Management::V26::Config do
   end
 
   describe "#configs" do
+    let(:project_id) { 48 }
+    let(:bot_id) { "test_bot" }
+
     it "Get configs" do
       VCR.use_cassette("/V26/config/index") do
-        configs = DocomoNlu::Management::V26::Config.all(params: { project_id: 1, bot_id: "test_bot" })
+        configs = DocomoNlu::Management::V26::Config.all(params: { project_id: project_id, bot_id: bot_id })
         expect(configs).not_to be nil
 
-        configs = DocomoNlu::Management::V26::Config.find(:all, params: { project_id: 1, bot_id: "test_bot" })
+        configs = DocomoNlu::Management::V26::Config.find(:all, params: { project_id: project_id, bot_id: bot_id })
         expect(configs).not_to be nil
 
-        configs = DocomoNlu::Management::V26::Config.where(project_id: 1, bot_id: "test_bot")
+        configs = DocomoNlu::Management::V26::Config.where(project_id: project_id, bot_id: bot_id)
         expect(configs).not_to be nil
       end
     end
 
     it "Update configs" do
       VCR.use_cassette("/V26/config/show") do
-        config = DocomoNlu::Management::V26::Config.find("", params: { project_id: 1, bot_id: "test_bot" })
+        config = DocomoNlu::Management::V26::Config.find("", params: { project_id: project_id, bot_id: bot_id })
         VCR.use_cassette("/V26/config/update") do
           config.test_Url = "https://example.com/api/"
           expect(config.save).to eq true
@@ -31,7 +34,7 @@ RSpec.describe DocomoNlu::Management::V26::Config do
 
     it "Delete configs" do
       VCR.use_cassette("/V26/config/show") do
-        config = DocomoNlu::Management::V26::Config.find("", params: { project_id: 1, bot_id: "test_bot" })
+        config = DocomoNlu::Management::V26::Config.find("", params: { project_id: project_id, bot_id: bot_id })
         VCR.use_cassette("/V26/config/delete") do
           expect(config.destroy(["test_Url"]).code).to eq "204"
         end
