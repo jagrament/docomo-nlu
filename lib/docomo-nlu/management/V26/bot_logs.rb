851beaa5f2c841b2e6a5928b5a33210b81093687
Koji Yamazaki <kouji.yamazaki.cv@nttdocomo.com>
2019-02-25 15:28:56 +0900

    Added classes

---
+++ lib/docomo-nlu/management/V26/bot_logs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module DocomoNlu
+  module Management::V26
+    class BotLogs < Base
+      self.element_name = "botlogs"
+      self.prefix = "/management/v2.6/projects/:project_id/"
+    end
+  end
+end
---
+++ lib/docomo-nlu/management/V26/logs.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module DocomoNlu
+  module Management::V26
+    class Logs < AIMLBase
+      self.element_name = "logs"
+      self.prefix = "/management/v2.6/projects/:project_id/"
+    end
+  end
+end
